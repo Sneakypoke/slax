8f7894e82dc10b03d2500589c33bd8b924a9e576
Michael Braude <misha.braude@gmail.com>
2025-02-12 13:03:47 +0200

    show timestamps in correct timezone

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -72,7 +72,12 @@ defmodule SlaxWeb.ChatRoomLive do
         </ul>
       </div>
       <div id="room-messages" class="flex flex-col grow overflow-auto" phx-update="stream">
-        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+        <.message
+          :for={{dom_id, message} <- @streams.messages}
+          dom_id={dom_id}
+          message={message}
+          timezone={@timezone}
+        />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -102,6 +107,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
@@ -111,8 +117,10 @@ defmodule SlaxWeb.ChatRoomLive do
         <div class="-mt-1">
           <.link class="text-sm font-semibold hover:underline">
             <span>{username(@message.user)}</span>
-            <span class="ml-1 text-xs text-gray-500">{message_timestamp(@message)}</span>
           </.link>
+          <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+            {message_timestamp(@message, @timezone)}
+          </span>
           <p class="text-sm">{@message.body}</p>
         </div>
       </div>
@@ -143,7 +151,9 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
 
-    {:ok, assign(socket, rooms: rooms)}
+    timezone = get_connect_params(socket)["timezone"]
+
+    {:ok, assign(socket, rooms: rooms, timezone: timezone)}
   end
 
   def handle_params(params, _uri, socket) do
@@ -206,8 +216,9 @@ defmodule SlaxWeb.ChatRoomLive do
     user.email |> String.split("@") |> List.first() |> String.capitalize()
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 end
